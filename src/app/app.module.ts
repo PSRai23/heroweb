505dfa24819a7f251ae63b5b873ba4a21cd086a7
Preetham Rai <rai.preethams@gmail.com>
2018-05-22 23:16:11 +0530

    export table added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExportTableComponent } from '../export-table/export-table.component'
+import { CarService } from '../services/carservice';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api'; 
+import {TableModule} from 'primeng/table';
+import { HttpClientModule }    from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExportTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule,
+    TableModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
